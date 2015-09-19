debbd3d22dbd71475a38145e9327e93be1d67d6a
Qianwen Luo <qianwenluo@email.arizona.edu>
2015-09-18 23:11:20 -0700

    new file

---
+++ hw3/same.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+#file: same.pl
+
+use strict;
+use warnings;
+
+my $var1 = shift;
+my $var2 = shift;
+
+if (! defined($var1) or ! defined($var2)) {
+    print "Please provide two strings.\n";
+  } elsif (lc($var1) eq lc($var2)) {
+    print "same\n";
+  } else {
+    print "different\n";
+  }
