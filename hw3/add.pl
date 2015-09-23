4e43181d33414a7124725b2baa6dc57f90861561
Ken Youens-Clark <kyclark@gmail.com>
2015-09-23 07:16:23 -0700

    perltidy

--- hw3/add.pl
+++ hw3/add.pl
@@ -1,14 +1,16 @@
-#!/user/bin/perl
+#!/usr/bin/perl
 #file: add.pl
 use strict;
 use warnings;
 my $x = shift;
 my $y = shift;
 
-if (! defined($x) or ! defined($y) ) {
-     print "please provide two numbers\n";
-} elsif ($x < 0 or $y < 0) {
+if ( !defined($x) or !defined($y) ) {
+    print "please provide two numbers\n";
+}
+elsif ( $x < 0 or $y < 0 ) {
     print "Please provide two positive numbers\n";
-} else{
+}
+else {
     print $x + $y, "\n";
 }
