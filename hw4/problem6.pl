6e9fa9d32b9ffba9f41f619c9e74e9889351f578
Qianwen Luo <qianwenluo@email.arizona.edu>
2015-09-24 10:02:32 -0700

    new file

--- hw4/problem6.pl
+++ hw4/problem6.pl
@@ -11,14 +11,22 @@ my $out = 'myresult.txt';
 open my $in_fh, '<', $in;
 open my $out_fh, '>', $out;
 
-while (my $line = <$in_fh>) {
-    if ($line % 2 == 0) {
-        if ($line < 24) {
-            print $line, "\n";
-        }
+while (my $n = <$in_fh>) {
+    chomp($n);
+    next unless $n;
+    if ($n % 2 == 0) {
+       if ($n <= 24) {
+         chomp($n);
+         print $n, "\n";
+       }
     }
     else {
-        my $fac = &fact( $line,1);
-        print "factorial: $fac\n";
+        my $factorial = 1;
+        
+        for (my $i = 1; $i <= $n; $i++){
+            $factorial *= $i;
+        }
+        
+        print $out_fh $factorial, "\n";
     }
 }
