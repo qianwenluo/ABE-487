7fdea1d99a7c3b6d895c20a4641902edf5783660
Qianwen Luo <qianwenluo@email.arizona.edu>
2015-10-07 11:36:07 -0700

    new file

---
+++ week5/08-snpas.pl
@@ -0,0 +1,31 @@
+#!/usr/bin env perl
+#file: 08-snps.pl
+
+use strict;
+use warnings;
+use autodie;
+
+if (@ARGV != 2) {
+    die "Please provide two sequences.\n";
+}
+
+my ($seq1, $seq2) = @ARGV;
+
+if (length($seq1) != length($seq2)){
+	die "Please ensure the sequences are the same length.\n";
+}
+
+my $count = 0;
+ 
+for (my $i = 0; $i < length($seq1); $i++) {
+    my $s1 = substr($seq1, $i, 1);
+    my $s2 = substr($seq2, $i, 1);
+   # print "i ($i), s1 ($s1), s2 ($s2)\n";
+
+    if ($s1 ne $s2) {
+        print "Pos ", $i+1, ": ", $s1, " => ", $s2, "\n";
+        $count++;
+    }
+}
+
+printf "Found %s SNP%s.\n", $count, ($count == 1) ? '' : 's';
