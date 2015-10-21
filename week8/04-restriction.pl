b6b3d574a2ae966be50d6b3c7e395962781b3c37
Qianwen Luo <qianwenluo@email.arizona.edu>
2015-10-21 10:01:15 -0700

    new file

---
+++ week8/04-restriction.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+#file: 04-restriction.pl
+
+use strict;
+use warnings;
+
+my $file = shift or die "Please provide a sequence or file.\n";
+
+open my $fh, '<', $file;
+
+
+while (my $line = <$fh>){
+    chomp($line);
+    $line =~ s/([AG])(AATT[CT])/$1^$2/g;
+    print $line;
+}
+
