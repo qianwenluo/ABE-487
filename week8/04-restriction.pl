bf1d1ff1351353be2498f139fe09cb16242f099c
Qianwen Luo <qianwenluo@email.arizona.edu>
2015-10-21 11:43:39 -0700

    fixed

--- week8/04-restriction.pl
+++ week8/04-restriction.pl
@@ -9,10 +9,11 @@ my $file = shift or die "Please provide a sequence or file.\n";
 
 open my $fh, '<', $file;
 
+my $str = '';
 while (my $line = <$fh>){
     chomp($line);
-    my $str .= $line;
-    $str =~ s/([AG])(AATT[CT])/$1^$2/g;
-    print $str;
+    $str .= $line;
 }
 
+$str =~ s/([AG])(AATT[CT])/$1^$2/g;
+print "$str\n";
